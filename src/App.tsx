eee0cab2780ebc886626bba4fd8f4305d4aacf3b
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2022-12-03 20:42:35 +0300

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <h1>Ton Sample TWA</h1>
+        <h1>TON Bank</h1>
         <TonConnector />
       </div>
     </QueryClientProvider>
